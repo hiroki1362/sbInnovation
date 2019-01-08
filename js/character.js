97ad8d266bc965f0f0ae85bf65a1fdd3c2a814dd
infodex-fujioka <hiroki.fujioka@infodex.co.jp>
2019-01-08 15:09:40 +0900

    Set Animation.

--- js/character.js
+++ js/character.js
@@ -1,10 +1,15 @@
-let mesh, camera, scene, renderer;
+let mesh, camera, scene, renderer, effect;
 let canvasWidth = 640;
 let canvasHeight = 430;
+let helper, ikHelper, physicsHelper;
+let clock = new THREE.Clock();
+
+const modelFile = "./img/cocon/cocon_v101.pmx";
+const motionFile = ["./img/motion/test.vmd"];
 
 $(function() {
 	initCharacter();
-	renderCharacter();
+	animateCharacter();
 });
 
 //シーン初期化処理
@@ -18,21 +23,25 @@ function initCharacter() {
 	//画面表示
 	renderer = new THREE.WebGLRenderer({
 		alpha: true,
-		canvas: document.querySelector("#characterCanvas")
+		canvas: document.querySelector("#characterCanvas"),
+		antialias: true
 	});
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(canvasWidth, canvasHeight);
 	renderer.setClearColor(0x000000, 0.5);
 
+	effect = new THREE.OutlineEffect(renderer);
+
 	//カメラ
 	camera = new THREE.PerspectiveCamera(40, canvasWidth / canvasHeight, 1, 1000);
-	//camera.position.set(0, 5, 22);
-	camera.position.set(0, 10.5, 17.5);
-	camera.rotation.x = -0.3;
+	camera.position.set(0, 10.5, 18.5);
+	//camera.rotation.x = 1.1;
 
+	helper = new THREE.MMDAnimationHelper({
+		afterglow: 2.0
+	});
 
 	let loader = new THREE.MMDLoader();
-	let modelFile = "./img/cocon/cocon_v101.pmx";
 
 	let onProgress = function(xhr) {
 		console.log("onProgress: " + xhr);
@@ -42,22 +51,44 @@ function initCharacter() {
 		console.log("error: " + xhr);
 	}
 
-	loader.load(
+	loader.loadWithAnimation(
 		modelFile,
-		function(object) {
-			mesh = object;
-			mesh.position.set(0, -1.5, 0);
-			mesh.rotation.set(0, 0, 0);
+		motionFile,
+		function(mmd) {
+			mesh = mmd.mesh;
+			mesh.position.set(0, -6.5, 1);
+			mesh.rotation.set(-0.2, 0, 0);
+			mesh.castShadow = true;
+			mesh.receiveShadow = true;
 			scene.add(mesh);
-		},
-		onProgress,
-		onError
-	);
+			
+			helper.add(mesh, {
+				animation: mmd.animation,
+				physics: true
+			});
+
+			ikHelper = helper.objects.get(mesh).ikSolver.createHelper();
+			ikHelper.visible = false;
+			scene.add(ikHelper);
+
+			physicsHelper = helper.objects.get(mesh).physics.createHelper();
+			physicsHelper.visible = false;
+			scene.add(physicsHelper);
+
+			//helper.setAnimation(mesh);
+			//helper.setPhysics(mesh);
+			
+			let controls = new THREE.OrbitControls(camera, renderer.domElement);
+		}, onProgress, onError);
 }
 
+//アニメーション
+function animateCharacter() {
+	requestAnimationFrame(animateCharacter);
+	renderCharacter();
+}
 //レンダリング
 function renderCharacter() {
-	requestAnimationFrame(renderCharacter);
-	renderer.clear();
-	renderer.render(scene, camera);
+	helper.update(clock.getDelta());
+	effect.render(scene, camera);
 }
